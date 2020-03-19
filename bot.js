a61df435ce88b226e3b629d25ad11a6afc2a535b
Kevin Hulen <kthulen@ucdavis.edu>
2020-03-18 17:02:09 -0700

    updated !remind, started !vote
    changed some apostrophes to quotes, changed var to let

--- bot.js
+++ bot.js
@@ -11,35 +11,55 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-    if (msg.content === '!hi') {
-        msg.reply('Hi!');
+    if (msg.content === "!hi") {
+        msg.reply("Hi!");
     }
 });
 
 client.on('message', msg => {
-    if (msg.content === '!cmd') {
+    if (msg.content === "!cmd") {
         msg.reply(`Commands:
             1. !hi:     bot says hi
             2. !remind: dm message to all users tagged
-                        usage: !remind [users] [date] [time] [message]
-                        example: !remind @guy,@dude,@homie 04/20/6969 14:00 howdy gamers
+                        usage: !remind [user1 user2...] [date] [time] [message]
+                        example: !remind @guy @dude @homie 04/20/6969 14:00 howdy gamers
+            3. !vote: Set up a vote. When not specified, the title is 'Poll', the options are 'Yay' and 'Nay', and the timer is 2 minutes.
+                      While a vote is ongoing, voters can add options and
+                      usage: !vote (-t title) (-o {option1} {option 2}...)
+                      example: !vote -s Who to kick from the island -t 5:00 -o {Son Goku} {Naruto}
+                               **Voting has begun for 5:00**
+                               !vote -o Dinkleburg
+                               ***Dinkleburg*** **added as a candidate**
+                               !vote Son Goku
+                               !vote -end
+                               **Voting ended early**
             `);
     }
 });
 
+// reminder system
 client.on('message', msg => {
-    var cmd = msg.content.split(" ");
-    var data = new Array (3)
+    let cmd = msg.content.split(" ");
+    let data = new Array (3)
     if (cmd[0] === "!remind") {
         try {
-            data[0] = cmd[1].split(",");
-            data[1] = cmd[2] + " " + cmd[3];
-            cmd = cmd.splice(4);
-            data[2] = cmd.join(' ');
+            let usrs = [];
+            let done = false;
+            let i = 1;
+            while(done === false) {
+                if (cmd[i].startsWith("<@!")) { usrs.push(cmd[i]); }
+                else { done = true; }
+                i++;
+            }
+            data[0] = usrs;
 
-            var date = new Date(data[1]);
+            data[1] = cmd[i+1] + " " + cmd[i+2];
+            cmd = cmd.splice(i+3);
+            data[2] = cmd.join(" ");
+
+            let date = new Date(data[1]);
             const reminder = new CronJob(date, function() {
-                for (var id of data[0]) {
+                for (let id of data[0]) {
                     id = id.replace(/[<@!>]/g,"");
                     client.users.fetch(id)
                         .then(u => u.createDM()
@@ -56,11 +76,22 @@ client.on('message', msg => {
             console.log("\nreminder command failed");
             console.error(e);
             msg.reply(`Mission failed. We'll get 'em next time.
-                usage: !remind [users] [date] [time] [message]
-                example: !remind @guy,@dude,@homie 04/20/6969 14:00 howdy gamers`);
+                usage: !remind [user1 user2...] [date] [time] [message]
+                example: !remind @guy @dude @homie 04/20/6969 14:00 howdy gamers`);
         }
     }
-})
+});
+
+// TODO: voting system
+// TODO: make sure each user's vote is counted once
+client.on('message', msg => {
+    let cmd = msg.content.split(" ");
+    if (cmd[0] === "!vote") {
+        let title = "Poll"
+        if (cmd.length > 1) { title = cmd.splice(1).join(" ") }
+
+    }
+});
 
 
 /*
